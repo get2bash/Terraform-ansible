f53a0532ded31f121c5430a5be4796363c4ef469
Isah Bashir Ibrahim <get2bash@gmail.com>
2023-02-04 04:00:54 +0100

    Delete main.tf

--- ec2/main.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_instance" "instances" {
-   count = 3   # Here we are creating 3 instances. 
-   ami = "ami-0778521d914d23bc1"
-   instance_type = "t2.micro"
-   key_name = "altschool"
-   tags = {
-      Name = "mini-project-${count.index}"
-   }
-}
